c884cfb4f61ed683412ac4c969058c00309b8b6c
Pedro Carvalho <plucas682@gmail.com>
2025-02-02 19:54:43 -0300

    Update models.py
    Alteração das classes Estado, Cidade, Contato, Adress e Pessoa, conforme as sugestões na UML do professor

--- registry/models.py
+++ registry/models.py
@@ -1,32 +1,102 @@
 from django.db import models
+from abc import ABC, abstractmethod
+import re
+import datetime
 
-class Municipio(models.Model):
-    nome = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.nome
-
-
-class Pessoa(models.Model):
-    tipo_doc = models.CharField(max_length=50)
-    doc_ident = models.CharField(max_length=50, primary_key=True)
-    id_mun = models.ForeignKey(Municipio, on_delete=models.CASCADE, related_name="pessoas")
-    nome = models.CharField(max_length=255)
-    dt_nac = models.DateField(null=True, blank=True)
-    email = models.EmailField(null=True, blank=True)
-    ddd = models.CharField(max_length=5, null=True, blank=True)
-    celular = models.CharField(max_length=15, null=True, blank=True)
+class Estado(models.Model):
+    id_estado = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=30)
+    uf = models.CharField(max_length=30)
 
     def __str__(self):
-        return self.nome
+        return f"{self.nome}"
 
 
-class Endereco(models.Model):
-    id_pes = models.OneToOneField(Pessoa, on_delete=models.CASCADE, primary_key=True, related_name="endereco")
+class Cidade(models.Model):
+    id_cidade = models.AutoField(primary_key=True)
+    estado = models.OneToOneField(Estado, on_delete=models.CASCADE, related_name="cidade")
+    nome = models.CharField(max_length=60)
+    cod_ibge = models.IntegerField
+
+    def __str__(self):
+        return f"{self.nome} - {self.estado.nome}"
+
+
+class Contato(models.Model):
+    id_contato = models.AutoField(primary_key=True)
+    contato_escolhas = [
+        ("telefone", "Telefone"),
+        ("email", "Email")
+    ]
+    tipo = models.CharField(max_length=10, choices=contato_escolhas)
+    chave = models.CharField(max_length=25, null=False, blank=False)
+
+    def __str__(self):
+        return f"{self.chave}"
+
+
+class Adress(models.Model):
+    id_adress = models.AutoField(primary_key=True)
+    adress_escolhas = [
+        ("residencial", "Residencial"),
+        ("fiscal", "Fiscal"),
+        ("comercial", "Comercial")
+    ]
+    tipo = models.CharField(max_length=20, choices=adress_escolhas, default="residencial")
     logradouro = models.CharField(max_length=255)
+    bairro = models.CharField(max_length=50) # Não sei se deixar ou não
     numero = models.CharField(max_length=10)
-    bairro = models.CharField(max_length=100)
     cep = models.CharField(max_length=20)
+    cidade = models.OneToOneField(Cidade, on_delete=models.CASCADE, related_name="adress")
 
     def __str__(self):
-        return f"{self.logradouro}, {self.numero}"
+        return f"{self.logradouro}, {self.numero}, {self.bairro}, {self.cidade.nome}"
+
+
+class Pessoa(models.Model, ABC):
+    id_pessoa = models.AutoField(primary_key=True)
+    contato = models.ManyToManyField(Contato, related_name="pessoa")
+    adress = models.ManyToManyField(Adress, related_name="pessoa")
+    data_cadastro = models.DateField(auto_now_add=True)
+
+    @abstractmethod
+    def validar_doc(self):
+        pass
+    
+    def validar_contato(self, contato: Contato):
+        if not isinstance(contato, Contato) or contato.tipo not in ["telefone", "email"] or self.contato.filter(chave=contato.chave).exists():
+            raise ValueError
+        if contato.tipo == "telefone":
+            if not re.fullmatch(r'\d{9,15}', contato.chave):
+                raise ValueError
+        elif contato.tipo == "email":
+            if not re.fullmatch(r'[^@]+@[^@]+\.[^@]+', contato.chave):
+                raise ValueError
+        return True
+
+    def set_contato(self, contato: Contato):
+        if self.validar_contato(contato) is True:
+            self.contato.add(contato)
+    
+    def get_contato(self):
+        return self.contato.all
+
+    def validar_adress(self, adress: Adress):
+        if not isinstance(adress, Adress) or self.adress.filter(logradouro=adress.logradouro, numero=adress.numero).exists() or not re.fullmatch(r'\d{5}-\d{3}|\d{8}', adress.cep):
+            raise ValueError
+        if not (adress.logradouro.strip() and adress.numero.strip() and adress.cep.strip()):
+            raise ValueError
+        return True
+    
+    def set_adress(self, adress: Adress):
+        if self.validar_adress(adress) is True:
+            self.adress.add(adress)
+    
+    def get_adress(self):
+        return self.adress.all
+    
+    def set_data_cadastro(self):
+        self.data_cadastro = datetime.datetime.now().strftime("%x")
+    
+    def get_data_cadastro(self):
+        return self.data_cadastro
