982ba790ebfad59bf61251bfd84f8d25ad98b9bc
Yuri Argolo <yuri.argolo1@hotmail.com>
2025-02-03 20:54:34 -0300

    Renomear classes de visualização para inglês: MunicipioViewSet, PessoaViewSet e EnderecoViewSet

--- registry/views.py
+++ registry/views.py
@@ -4,16 +4,16 @@ from rest_framework import viewsets
 from .models import Municipality, Person, Address
 from .serializers import MunicipalitySerializer, PersonSerializer, AddressSerializer
 
-class MunicipioViewSet(viewsets.ModelViewSet):
+class MunicipalityViewSet(viewsets.ModelViewSet):
     queryset = Municipality.objects.all()
     serializer_class = MunicipalitySerializer
 
 
-class PessoaViewSet(viewsets.ModelViewSet):
+class PersonViewSet(viewsets.ModelViewSet):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
 
 
-class EnderecoViewSet(viewsets.ModelViewSet):
+class AddressViewSet(viewsets.ModelViewSet):
     queryset = Address.objects.all()
     serializer_class = AddressSerializer
