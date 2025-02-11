a2a3e8fcf12a9987e04e99866c4d9d7f1557c66b
Pedro Carvalho <plucas682@gmail.com>
2025-02-10 23:08:44 -0300

    Update contact.py
    Classe contact atualizada para acomodar validate_contact.py

--- registry/models/contact.py
+++ registry/models/contact.py
@@ -1,42 +1,29 @@
 from django.db import models
 import re
+from .validate_contact import ContactImplementation
 
 class Contact(models.Model):
     id_contact = models.AutoField(primary_key=True)
     contact_choices = [
         ("phone", "Telefone"),
         ("email", "Email")
-    ]
+        ]
     contact_type = models.CharField(max_length=10, choices=contact_choices)
     contact_key = models.CharField(max_length=50, null=False, blank=False)
 
     def __str__(self):
         return f"{self.contact_key}"
     
-    def validate_contact_type(self, contact_type: str):
-        if contact_type not in dict(self.contact_choices).keys():
-            raise ValueError
-        return True
-    
     def set_contact_type(self, contact_type: str):
-        if self.validate_contact_type(contact_type) is True:
+        if ContactImplementation.validate_contact_type(contact_type):
             self.contact_type = contact_type
             self.save()
 
     def get_contact_type(self):
         return self.contact_type
-    
-    def validate_contact_key(self, contact_key: str):
-        if not contact_key.strip():
-            raise ValueError
-        if self.contact_type == "phone" and not re.fullmatch(r'\d{8,15}', contact_key):
-            raise ValueError
-        if self.contact_type == "email" and not re.fullmatch(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', contact_key):
-            raise ValueError
-        return True
 
     def set_contact_key(self, contact_key: str):
-        if self.validate_contact_key(contact_key) is True:
+        if ContactImplementation.validate_contact_key(contact_key, self.contact_type):
             self.contact_key = contact_key
             self.save()
 
