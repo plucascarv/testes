57e4f92a2f6a1e6fce6217ae90083ded15f6d1cc
Fernando Barreto Costa <costa.fernando@ufba.br>
2025-02-12 19:04:47 -0300

    fix: serializers

--- registry/serializers.py
+++ registry/serializers.py
@@ -1,48 +1,50 @@
 from rest_framework import serializers
-from .models import Municipality, Person, Address, IndividualPerson, BusinessPerson
-from .models.individual_person import IndividualPerson
+from .models import (
+    Municipality,
+    Person,
+    Address,
+    IndividualPerson,
+    BusinessPerson,
+)
 
 
 class MunicipalitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Municipality
-        fields = '__all__'
+        fields = "__all__"
+
 
 class PersonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Person
-        fields = '__all__'
+        fields = "__all__"
+
 
 class AddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = Address
-        fields = '__all__'
+        fields = "__all__"
 
-# class IndividualSerializer(PersonSerializer):  # Herda de PersonSerializer
-#     class Meta:
-#         model = IndividualPerson
-#         fields = '__all__'
-        
-class IndividualSerializer(PersonSerializer):  
+
+class IndividualSerializer(PersonSerializer):
     class Meta:
         model = IndividualPerson
-        fields = '__all__'
+        fields = "__all__"
 
     def to_representation(self, instance):
         data = super().to_representation(instance)
-        request = self.context.get('request')
+        request = self.context.get("request")
 
         # Exemplo: ocultar CPF e RG para usuários não autenticados
         if request and not request.user.is_authenticated:
-            data.pop('cpf', None)
-            data.pop('rg', None)
-            data.pop('pis', None)
+            data.pop("cpf", None)
+            data.pop("rg", None)
+            data.pop("pis", None)
 
         return data
-    
+
 
 class BusinessSerializer(PersonSerializer):  # Herda de PersonSerializer
     class Meta:
-        model = businessperson.BusinessPerson
-        fields = '__all__'
-        
+        model = BusinessPerson
+        fields = "__all__"
